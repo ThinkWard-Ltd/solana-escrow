8fa8662836e887d475819a59bbf883f6303ca9bc
Michael K <thkoon@connect.ust.hk>
2021-05-30 23:21:30 +0800

    Fix bugs & Add processor.rs

--- src/instruction.rs
+++ src/instruction.rs
@@ -1,10 +1,9 @@
 use std::convert::TryInto;
 use solana_program::program_error::ProgramError;
 
-use crate::error::EscrowError;::InvalidInstruction;
+use crate::error::EscrowError::InvalidInstruction;
 
- pub enum EscrowInstruction {
-
+pub enum EscrowInstruction {
     // Starts the trade by creating and populating an escrow account and transferring ownership of the given temp token account to the PDA
     //
     //
@@ -25,14 +24,14 @@ use crate::error::EscrowError;::InvalidInstruction;
 
 impl EscrowInstruction {
     // Unpacks a byte buffer into a [EscrowInstruction](enum.EscrowInstruction.html).
-    pub fn unpack(input: &[u8] -> Result<Self, ProgramError>) {
+    pub fn unpack(input: &[u8]) -> Result<Self, ProgramError> {
         let (tag, rest) = input.split_first().ok_or(InvalidInstruction)?;
 
         Ok(match tag {
             0 => Self::InitEscrow {
                 amount: Self::unpack_amount(rest)?,
             },
-            _ => return Err(InvalidInstruction.info()),
+            _ => return Err(InvalidInstruction.into()),
         })
     }
 
